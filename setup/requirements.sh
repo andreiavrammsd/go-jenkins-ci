16b922935c9adabf7710b47c08ece74b5b6ea8ce
Andrei Avram <avramandrei@ymail.com>
2019-02-14 21:54:53 +0200

    Use sh.

--- setup/requirements.sh
+++ setup/requirements.sh
@@ -39,6 +39,6 @@ f=/usr/local/bin/docker-compose
 curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o ${f}
 chmod +x ${f}
 
-if [[ ${U} != "root" ]]; then
+if [ ${U} != "root" ]; then
     exit
 fi
